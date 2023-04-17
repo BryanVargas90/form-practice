6abeb490777f1f9ca5c344ad126611db24e6aa45
bryandanielpsp@gmail.com <bryandanielpsp@gmail.com>
2023-04-17 16:40:25 -0400

    passing props

---
+++ README.md
@@ -0,0 +1,16 @@
+# Form Practice - React.js App
+Building a React app with onchange functionality for an HTML form for BocaCode c-10 in class practice.
+
+![screen shot](./public/images/Readme.png)
+
+## Covered In class
+* onChange
+* Form Elements
+* Controlled Components
+
+## Basic code
+```
+<label> First Name:
+<input type="text">
+</label>
+```
---
+++ src/components/Form.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+
+export default function Form() {
+const[firstName, setFirstName] = useState();
+const[lastName, setLastName] = useState();
+const[color,setColor] = useState("#00ff00")
+const[datelocal,setDateLocal]=useState()
+const[comment, setComment]= useState()
+
+const handleUpdate = (e) => {
+    const newValue = e.target.value;
+    console.log(newValue.trim)
+}
+
+return (
+    <form>
+         <h2>Form</h2>
+        <label> First Name:
+        <input type="text" 
+        value={firstName}
+        placeholder="First Name"
+         onChange={ handleUpdate} />
+    </label>
+    
+    <br />
+  
+    <label>Last Name:
+    <input type="text"
+    value={lastName}
+    placeholder="Last Name"
+    onChange={(handleUpdate)} />
+    </label>
+
+    <br />
+    <label>Color:
+        <input type="color"
+        value={color}
+        onChange={handleUpdate}/>
+      </label>
+
+      <br/>
+
+      <label> Date:
+        <input type="datetime-local"
+        value={datelocal}
+        onChange={ handleUpdate } />
+      </label>
+
+      <br/ >
+    <label>Message:
+    <textarea 
+    col="5" 
+    row="25" 
+    onChange={handleUpdate}>{comment}</textarea>
+    </label>
+
+    </form>
+ )
+}
